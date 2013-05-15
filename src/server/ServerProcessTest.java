cf89eeab4718b644859252d1a3293903211f0d20
Jan <jkr241@mit.edu>
2013-05-15 02:40:08 -0400

    complete testing of server Process

--- src/server/ServerProcessTest.java
+++ src/server/ServerProcessTest.java
@@ -30,15 +30,9 @@ public class ServerProcessTest {
 			processor.addLine("username gabo", writer);
 			String response;
 			
-			while (true) {
+			while((response =in.readLine()) == null){
 				//flush writer -> write message
 				writer.flush();
-				//read file
-				response = in.readLine();
-				//if something was written, break
-				if (response != null) {
-					break;
-				}
 			}
 			
 			//close writer
@@ -69,36 +63,34 @@ public class ServerProcessTest {
 			//writer and reader for file
 			PrintWriter writer = new PrintWriter(new FileWriter("src/server/test.txt", false));
 			BufferedReader in = new BufferedReader(new FileReader("src/server/test.txt"));
-
+			String response;
+			
 			//command through server processor
 			processor.addLine("username jan", writer);
-			while (true) {
+			
+			while((response =in.readLine()) == null){
 				//flush writer -> write message
 				writer.flush();
-				//if something was written, break
-				if (in.readLine() != null) {
-					break;
-				}
 			}
+			
+			//checking if correct message was sent
+			assertTrue(response.replaceAll("\\s+", "").equals("startjan?"));
+			
 			//erasing file
 			FileOutputStream eraser = new FileOutputStream("src/server/test.txt");
 			eraser.write((new String()).getBytes());
 			eraser.close();
 		
 			processor.addLine("new jan", writer);
-			String response2;
-			while (true) {
+			
+			while((response =in.readLine()) == null){
 				//flush writer -> write message
 				writer.flush();
-				//read file
-				response2 = in.readLine();
-				//if something was written, break
-				if (response2 != null) {
-					break;
-				}
 			}
 			
-			assertTrue(response2.replaceAll("\\s+", "").equals("newjan0"));
+			//checking if correct message was sent
+			assertTrue(response.replaceAll("\\s+", "").equals("newjan0"));
+			
 			//close reader
 			in.close();
 			writer.close();
@@ -128,39 +120,40 @@ public class ServerProcessTest {
 
 			//command through server processor
 			processor.addLine("username jon", writer);
+			
 			String response;
-			while (true) {
+			
+			while ((response = in.readLine())==null) {
 				//flush writer -> write message
 				writer.flush();
-				//if something was written, break
-				if (in.readLine() != null) {
-					break;
-				}
 			}
+			//checking for correct response
+			assertTrue(response.replaceAll("\\s+", "").equals("startjon?"));
+			
 			//erasing file
 			FileOutputStream eraser = new FileOutputStream("src/server/test.txt");
 			eraser.write((new String()).getBytes());
 			eraser.close();
 		
-			processor.addLine("new jon", writer);	
-			while (true) {
+			processor.addLine("new jon", writer);
+			
+			while ((response = in.readLine())== null) {
 				//flush writer -> write message
 				writer.flush();
-				
-				//if something was written, break
-				if (in.readLine() != null) {
-					break;
-				}
-			}			
-			while (true) {
-				//flush writer -> write message
-				writer.flush();
-				
-				//if something was written, break
-				if (in.readLine() != null) {
-					break;
-				}
 			}
+			
+			//checking for correct response
+			assertEquals(response, "new jon 0");
+			
+			while ((response = in.readLine())== null) {
+				//flush writer -> write message
+				writer.flush();
+
+			}
+			
+			//checking if correct message was sent
+			assertEquals(response, "create 0");
+			
 			//erasing file
 			FileOutputStream eraser1 = new FileOutputStream("src/server/test.txt");
 			eraser1.write((new String()).getBytes());
@@ -169,61 +162,54 @@ public class ServerProcessTest {
 			PrintWriter chauWriter = new PrintWriter(new FileWriter("src/server/test1.txt", false));
 			BufferedReader inChau = new BufferedReader(new FileReader("src/server/test1.txt"));
 
-			processor.addLine("username chau", chauWriter);	
-			while (true) {
+			processor.addLine("username chau", chauWriter);
+			
+			while ((response = inChau.readLine())== null) {
 				//flush writer -> write message
 				chauWriter.flush();
-				//if something was written, break
-				if (inChau.readLine() != null) {
-					break;
-				}
 			}
 			
+			//checking if correct message was sent
+			assertEquals(response, "start chau jon ? 0");
+			
+			//erasing file
 			FileOutputStream eraser2 = new FileOutputStream("src/server/test1.txt");
 			eraser2.write((new String()).getBytes());
 			eraser2.close();
-
+			
 			processor.addLine("invite chau 0", writer);
-			while (true) {
+			
+			while ((response = in.readLine())==null) {
 				//flush writer -> write message
 				writer.flush();
-				response = in.readLine();
-				//if something was written, break
-				if (response != null) {
-					break;
-				}
 			}
 			
-			while (true) {
+			//checking if correct message was sent
+			assertEquals(response, "connect chau");
+			
+			while ((response = in.readLine())==null) {
 				//flush writer -> write message
 				writer.flush();
-				response = in.readLine();
-				//if something was written, break
-				if (response != null) {
-					break;
-				}
 			}
-
-			assertTrue(response.replaceAll("\\s+", "").equals("addchau0"));
 			
+			//checking if correct message was sent
+			assertEquals(response, "add chau 0");
+			
+			//erasing file
 			FileOutputStream eraser3 = new FileOutputStream("src/server/test.txt");
 			eraser3.write((new String()).getBytes());
 			eraser3.close();
 			
-			while (true) {
+			while ((response = inChau.readLine())==null) {
 				//flush writer -> write message
 				chauWriter.flush();
-				response = inChau.readLine();
-				//if something was written, break
-				if (response != null) {
-					break;
-				}
 			}
 		
 			assertTrue(response.replaceAll("\\s+", "").equals("newjonchau?0"));
 
 			//closer reader
 			in.close();
+			inChau.close();
 			writer.close();
 			//clear file contents
 			FileOutputStream eraser4 = new FileOutputStream("src/server/test1.txt");
@@ -235,5 +221,223 @@ public class ServerProcessTest {
 			throw new RuntimeException();
 		}
 	}
-	
+	//timeout ensures we don't loop infinitely
+	@Test (timeout = 1000)
+	public void testDisconnect(){
+		//start server processor
+		ServerProcess processor = new ServerProcess();
+		processor.start();
+		
+		try {
+			//writer and reader for file
+			PrintWriter writer = new PrintWriter(new FileWriter("src/server/test.txt", false));
+			BufferedReader in = new BufferedReader(new FileReader("src/server/test.txt"));
+
+			//command through server processor
+			processor.addLine("username gabo", writer);
+			String response;
+			
+			while((response =in.readLine()) == null){
+				//flush writer -> write message
+				writer.flush();
+			}
+			//Checking correct message
+			assertEquals(response, "start gabo ? ");
+			//erasing file
+			FileOutputStream eraser = new FileOutputStream("src/server/test.txt");
+			eraser.write((new String()).getBytes());
+			eraser.close();
+			
+			processor.addLine("disconnect gabo", writer);
+			
+			
+			while((response =in.readLine()) == null){
+				//flush writer -> write message
+				writer.flush();
+			}
+			assertEquals(response, "disconnect gabo");
+			FileOutputStream eraser2 = new FileOutputStream("src/server/test.txt");
+			eraser2.write((new String()).getBytes());
+			eraser2.close();
+			//Closing writer and reader
+			in.close();
+			writer.close();
+		} catch (IOException e){
+			System.out.println(e.getMessage());
+			throw new RuntimeException();
+		}
+	}
+	//timeout ensures we don't loop infinitely
+	@Test (timeout = 1000)
+	public void testUsedname(){
+		//start server processor
+		ServerProcess processor = new ServerProcess();
+		processor.start();
+		
+		try {
+			//writer and reader for file
+			PrintWriter writer = new PrintWriter(new FileWriter("src/server/test.txt", false));
+			BufferedReader in = new BufferedReader(new FileReader("src/server/test.txt"));
+
+			//command through server processor
+			processor.addLine("username gabo", writer);
+			String response;
+			
+			while((response =in.readLine()) == null){
+				//flush writer -> write message
+				writer.flush();
+			}
+			//Checking correct message
+			assertEquals(response, "start gabo ? ");
+			//erasing file
+			FileOutputStream eraser = new FileOutputStream("src/server/test.txt");
+			eraser.write((new String()).getBytes());
+			eraser.close();
+			
+			//adding same username, before other disconnects
+			processor.addLine("username gabo", writer);
+			
+			while((response =in.readLine()) == null){
+				//flush writer -> write message
+				writer.flush();
+			}
+			//Checking correct message
+			assertEquals(response, "abort");
+			
+			//erasing file
+			FileOutputStream eraser2 = new FileOutputStream("src/server/test.txt");
+			eraser2.write((new String()).getBytes());
+			eraser2.close();
+			
+			//Closing reader and writer
+			in.close();
+			writer.close();
+			
+		} catch (IOException e){
+			System.out.println(e.getMessage());
+			throw new RuntimeException();
+		}		
+	}
+	//timeout ensures we don't loop infinitely
+	@Test (timeout = 1000)
+	public void testLeave() {
+		//start server processor
+		ServerProcess processor = new ServerProcess();
+		processor.start();
+		
+		try {
+			//writer and reader for file
+			PrintWriter writer = new PrintWriter(new FileWriter("src/server/test.txt", false));
+			BufferedReader in = new BufferedReader(new FileReader("src/server/test.txt"));
+
+			//command through server processor
+			processor.addLine("username gabo", writer);
+			String response;
+			
+			while((response =in.readLine()) == null){
+				//flush writer -> write message
+				writer.flush();
+			}
+			//Checking correct message
+			assertEquals(response, "start gabo ? ");
+			//erasing file
+			FileOutputStream eraser = new FileOutputStream("src/server/test.txt");
+			eraser.write((new String()).getBytes());
+			eraser.close();
+			
+		
+			processor.addLine("new gabo", writer);
+					
+			while ((response = in.readLine())== null) {
+				//flush writer -> write message
+				writer.flush();
+			}
+			
+			//checking for correct response
+			assertEquals(response, "new gabo 0");
+			
+			//erasing file
+			FileOutputStream eraser2 = new FileOutputStream("src/server/test.txt");
+			eraser2.write((new String()).getBytes());
+			eraser2.close();
+			
+			while((response =in.readLine()) == null){
+				//flush writer -> write message
+				writer.flush();
+			}
+			//Checking correct message
+			assertEquals(response, "create 0");
+			
+			//erasing file
+			FileOutputStream eraser3 = new FileOutputStream("src/server/test.txt");
+			eraser3.write((new String()).getBytes());
+			eraser3.close();
+			
+			PrintWriter chauWriter = new PrintWriter(new FileWriter("src/server/test1.txt", false));
+			BufferedReader inChau = new BufferedReader(new FileReader("src/server/test1.txt"));
+
+			processor.addLine("username chau", chauWriter);
+			
+			while ((response = inChau.readLine())== null) {
+				//flush writer -> write message
+				chauWriter.flush();
+			}
+			
+			//checking if correct message was sent
+			assertEquals(response, "start chau gabo ? 0");
+			
+			//erasing file
+			FileOutputStream eraserchau = new FileOutputStream("src/server/test1.txt");
+			eraserchau.write((new String()).getBytes());
+			eraserchau.close();
+			
+			processor.addLine("invite chau 0", writer);
+			
+			while ((response = in.readLine())==null) {
+				//flush writer -> write message
+				writer.flush();
+			}
+			
+			//checking if correct message was sent
+			assertEquals(response, "connect chau");
+			
+			while ((response = inChau.readLine())==null) {
+				//flush writer -> write message
+				chauWriter.flush();
+			}
+			assertEquals(response, "new gabo chau  ?  0");
+
+			while ((response = in.readLine())==null) {
+				//flush writer -> write message
+				writer.flush();
+			}
+			//checking if correct message was sent
+			assertEquals(response, "add chau 0");
+
+			//leave message to server
+			processor.addLine("leave gabo 0", writer);
+			
+			while((response =inChau.readLine()) == null){
+				//flush writer -> write message
+				chauWriter.flush();
+			}
+			//checking for correct response
+			assertEquals(response, "leave gabo 0");
+			
+			//erasing file
+			FileOutputStream eraser4 = new FileOutputStream("src/server/test.txt");
+			eraser4.write((new String()).getBytes());
+			eraser4.close();
+			
+			//Closing writer and reader
+			in.close();
+			inChau.close();
+			writer.close();
+			
+			
+		} catch (IOException e){
+			System.out.println(e.getMessage());
+			throw new RuntimeException();
+		}	
+	}
 }
